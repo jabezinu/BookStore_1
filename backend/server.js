a01935bd9c1a251a599fccdb17f002c8e7bf3832
jabezinu <jabezzinu31@gmail.com>
2025-05-31 04:10:42 +0300

    done with the CRUD functionality

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,88 @@
 import express from 'express';
-import Book from './models/BooksModel.js';
+import mongoose from 'mongoose';
+import { MONGO_URI, PORT } from './config.js';
+import { Book } from './models/BooksModel.js';
 
 const app = express();
-const PORT = 5555;
+const port = PORT || 5000;
+app.use(express.json())
 app.post('/books', async (req, res) => {
     try {
-        const {title, author, publishedYear} = req.body;
-        const book = await Book.create({
-            title,
-            author,
-            publishedYear,
-        })
-        console.log('test')
-        if(book){
-            return res.status.json({message: "book was successfully created!"})
-        }        
+        if(!req.body.title || !req.body.publishedYear || !req.body.author ){
+            res.status(400).send({message: "Send all required fields"})
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishedYear: req.body.publishedYear,
+        }
+
+        const book = await Book.create(newBook)
+
+        return res.status(201).send(book)     
     } catch (error) {
         console.log(error)
         res.status(500).json({message: "error while createing the book"})
     }
 })
-app.get('/books', (req, res) => {
-    res.status(200).json({Book})
+
+app.get('/books', async (req, res) => {
+    try {
+        const book = await Book.find()
+        res.status(200).json(book)        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "error while GETing all book"})
+    }
 })
-app.listen(PORT, () => {
-    console.log(`server is runinng on ${PORT}`)
+
+app.get('/books/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const book = await Book.findById(id)
+        res.status(200).json(book)        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "error while GETing book by ID"})
+    }
 })
+app.put('/books/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const updaedData = {
+            title: req.body.title,
+            author: req.body.author,
+            publishedYear: req.body.publishedYear,
+        }
+        const book = await Book.findByIdAndUpdate(id, updaedData, {new: true})
+        res.status(200).json(book)        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "error in the update route"})
+    }
+})
+
+app.delete('/books/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const book = await Book.findByIdAndDelete(id)
+        res.status(200).json({message : "Book Delted successfully"})        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "error in the delete route"})
+    }
+})
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("Database is connected");
+        app.listen(port, () => {
+            console.log(`server is runinng on ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+
+    })
