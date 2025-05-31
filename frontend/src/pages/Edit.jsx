1246b4146f977e21205efb848713bb41d22d4685
jabezinu <jabezzinu31@gmail.com>
2025-06-01 00:32:22 +0300

    the pure html of the page

--- frontend/src/pages/Edit.jsx
+++ frontend/src/pages/Edit.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const Edit = () => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [publishedYear, setPublishedYear] = useState()
+  const [loading, setLoading] = useState(false);
+  const {id} = useParams();
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`http://localhost:5555/books/${id}`)
+    .then((res) => {
+      setTitle(res.data.title)
+      setAuthor(res.data.author)
+      setPublishedYear(res.data.publishedYear)
+      setLoading(false)
+    })
+    .catch((err) => {
+      console.log(err)
+      setLoading(false)
+    })
+  },[])
+
+  const handleUpdateButton = () => {
+    setLoading(true)
+    axios
+    .put(`http://localhost:5555/books/${id}`, {title, author, publishedYear})
+    .then(() => {
+        setLoading(false)        
+    })
+    .catch((error) => {
+      setLoading(false)
+      alert("Something went wrong while updating check you console")
+      console.log(error)
+    })
+
+  }
   return (
     <div>
-      <h1>hello</h1>
+      <Link to={'/'}><IoMdArrowRoundBack /></Link>
+      <div>
+        <h1>Update Book Data</h1>
+        <label>Title: </label>
+        <input type='text' value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <label>Author: </label>
+        <input type='text' value={author} onChange={(e) => setAuthor(e.target.value)}/>
+        <label>Published Year: </label>
+        <input type='number' value={publishedYear} onChange={(e) => setPublishedYear(e.target.value)} />
+        <Link to={`/`}>
+          <button onClick={handleUpdateButton}>Update</button>
+        </Link>
+      </div>
     </div>
   )
 }
