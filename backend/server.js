66ba714d212895837eda0e6ee45d29a95d583a50
jabezinu <jabezzinu31@gmail.com>
2025-06-18 02:13:51 +0300

    integrate dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { MONGO_URI, PORT } from './config.js';
 import router from './routes/routes.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const port = PORT || 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json())
 
 app.use(cors())
 app.use('/books', router)
 
 mongoose
-    .connect(MONGO_URI)
+    .connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Database is connected");
         app.listen(port, () => {
